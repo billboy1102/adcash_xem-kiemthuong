1b1ce5ec3ef0e391686a8f6de9eec5556ae7562a
billboy1102 <mabasuke23@gmail.com>
2026-08-17 22:54:04 +0700

    Configure Android wrapper

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.bobbey.adcash',
+  appName: 'Adcash: Xem & Kiếm Thưởng',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+  },
+}
+
+export default config
