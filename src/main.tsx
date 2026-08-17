b4209f13caad92ead28045ee700ecf6db8346af8
billboy1102 <mabasuke23@gmail.com>
2026-08-18 05:11:34 +0700

    Use unified login with automatic admin routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import RootApp from './RootApp'
 import './styles.css'
 import './production.css'
+import './auth-overrides.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RootApp />
   </React.StrictMode>,
 )
